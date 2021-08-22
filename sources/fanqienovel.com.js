b474f2b97f1a9014a4cd891ae11abbea0ca3073f
kaitl <54618947+kaitl@users.noreply.github.com>
2021-08-22 14:03:48 +0800

    更改了番茄小说的搜索接口

--- sources/fanqienovel.com.js
+++ sources/fanqienovel.com.js
@@ -2,15 +2,15 @@ const baseUrl = "https://fanqienovel.com"
 
 //搜索
 const search = (key) => {
-  let response = GET(`${baseUrl}/api/author/search/search_book/v1?filter=127,127,127&page_count=10&query_word=${encodeURI(key)}`)
+  let response = GET(`https://api5-normal-lq.fqnovel.com/reading/bookapi/search/page/v/?offset=0&query=${encodeURI(key)}&iid=466614321180296&aid=1967`)
   let array = []
   let $ = JSON.parse(response)
-  $.data.search_book_data_list.forEach((child) => {
+  $.data.forEach((child) => {
     array.push({
-      name: child.book_name,
-      author: child.author,
-      cover: `https://p3-tt.byteimg.com/img/${child.thumb_uri}~240x312.jpg`,
-      detail: `${baseUrl}/page/${child.book_id}`,
+      name: child.book_data[0].book_name,
+      author: child.book_data[0].author,
+      cover: child.book_data[0].thumb_url.replace(".heic",".png"),
+      detail: `https://api5-normal-lq.fqnovel.com/reading/bookapi/detail/v/?book_id=${child.book_data[0].book_id}&iid=466614321180296&aid=1967&version_code=290`,
     })
   })
   return JSON.stringify(array)
@@ -19,17 +19,29 @@ const search = (key) => {
 //详情
 const detail = (url) => {
   let response = GET(url)
-  let $ = HTML.parse(response)
+  let $ = JSON.parse(response).data
   let book = {
-    summary: $('.page-abstract-content').text(),
-    status: $('.info-label-yellow').text(),
-    category: $('.info-label-grey:nth-child(3)').text(),
-    words: $('.info-count-word').text().replace("字",""),
-    update: $('.info-last-time').text(),
-    lastChapter: $('.info-last-title').text().replace("最近更新：",""),
-    catalog: `${baseUrl}/api/reader/directory/detail?bookId=${url.replace("https://fanqienovel.com/page/","")}`
+    summary: $.abstract,
+    status: $.update_status == 1 ? '连载' : '完结',
+    category: $.tags.replace(","," "),
+    words: $.word_number,
+    update: timestampToTime($.last_chapter_update_time),
+    lastChapter: $.last_chapter_title,
+    catalog: `${baseUrl}/api/reader/directory/detail?bookId=${$.book_id}`
   }
   return JSON.stringify(book)
+}
+
+//转换更新时间 时间戳
+function timestampToTime(timestamp) {
+        var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+        var Y = date.getFullYear() + '-';
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1):date.getMonth()+1) + '-';
+        var D = (date.getDate()< 10 ? '0'+date.getDate():date.getDate())+ ' ';
+        var h = (date.getHours() < 10 ? '0'+date.getHours():date.getHours())+ ':';
+        var m = (date.getMinutes() < 10 ? '0'+date.getMinutes():date.getMinutes()) + ':';
+        var s = date.getSeconds() < 10 ? '0'+date.getSeconds():date.getSeconds();
+        return Y+M+D+h+m+s;
 }
 
 //目录
@@ -62,7 +74,7 @@ const rank = (title, category, page) => {
       name: child.book_name,
       author: child.author,
       cover: `https://p3-tt.byteimg.com/img/${child.thumb_uri}~240x312.jpg`,
-      detail: `${baseUrl}/page/${child.book_id}`,
+      detail: `https://api5-normal-lq.fqnovel.com/reading/bookapi/detail/v/?book_id=${child.book_id}&iid=466614321180296&aid=1967&version_code=290`,
     })
   })
   return JSON.stringify({
@@ -89,6 +101,6 @@ const ranks = [
 var bookSource = JSON.stringify({
   name: "番茄小说",
   url: "fanqienovel.com",
-  version: 103,
+  version: 104,
   ranks: ranks
 })
